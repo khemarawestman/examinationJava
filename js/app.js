a068e3add2201170ac67ab96acbd7c8b11267b36
khemarawestman <erikkhemarawestman@hotmail.se>
2023-11-09 22:57:26 +0100

    small change

--- js/app.js
+++ js/app.js
@@ -1,50 +1,81 @@
-// Add interactivity to the planets with API integration
-
-const planets = document.querySelectorAll('.planet');
-const planetInfoElement = document.getElementById('planet-info');
-
-planets.forEach(planet => {
-  planet.addEventListener('click', handleClick);
+document.addEventListener('DOMContentLoaded', () => {
+  // Fetch planet data and set up click events when the document is ready
+  fetchPlanetDataAndSetup();
 });
 
-async function handleClick(event) {
+async function fetchPlanetDataAndSetup() {
+  const planetData = await fetchPlanetData();
+  if (planetData) {
+    setupClickEvents(planetData);
+    setupModalCloseEvents();
+  }
+}
+async function fetchPlanetData() {
   try {
-    const planet = event.target;
-    const planetId = planet.id;
-
-    const data = await fetchPlanetInfo(planetId);
-    console.log(data);
-
-    // Process the returned data
-    const planetName = data.name;
-    const latinName = data.latinName;
-    const rotationPeriod = data.rotation;
-
-    // Update the HTML content with the planet information
-    const planetInfo = `
-      <h3>Planet: ${planetName}</h3>
-      <p>Latin Name: ${latinName}</p>
-      <p>Rotation Period: ${rotationPeriod} hours</p>
-    `;
-    planetInfoElement.innerHTML = planetInfo;
-
+    const response = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
+      headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" }
+    });
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    console.log(data.bodies);
+    return data.bodies; // Returns the planet data
+    
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching data:", error);
   }
 }
 
-function fetchPlanetInfo(planetId) {
-  const apiUrl = `https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies/${id}`;
 
-  return fetch(apiUrl, {
-      headers: {
-        'x-zocom': 'solaris-B2mWxADrthdHqd22'
-      }
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch planet info');
-      }
-      return response.json();
-    });
+
+function setupClickEvents(planetsData) {
+  document.querySelectorAll('.planet').forEach(planetElement => {
+    planetElement.addEventListener('click', () => displayPlanetInfo(planetsData, planetElement.id));
+  });
 }
+
+function displayPlanetInfo(planetsData, planetId) {
+  const planet = planetsData.find(p => p.name.toLowerCase() === planetId);
+  if (!planet) return;
+
+  const planetInfoHtml =
+  `
+  <div class="planet-info">
+  <div class = containinfo>
+    <h3> ${planet.name}</h3>
+    <p class="latin-name">${planet.latinName}</p>
+    <p class="description"> ${planet.desc}</p>
+  </div>
+  <div class="planetinfo">
+  
+  </div>
+    <div class=footer >
+    <p class="circumference">CIRCUMFERENCE<br>${planet.circumference}</p>
+    <p class="rotation">Rotation Period<br> ${planet.rotation}</p>
+    <p class="day-temperature">MAX TEMPERATURE (Day)<br> ${planet.temp.day}°C</p>
+    
+    </div>
+    <div class="distance-from-sun">
+   
+    <p class="distance">DISTANCE FROM THE SUN <br> ${planet.distance} KM</p>
+    <p class="night-temperature">MAX TEMPERATURE (Night)<br> ${planet.temp.night}°C</p>
+   
+  </div>
+
+  </div>`;
+  const planetInfoElement = document.getElementById('planet-info');
+  planetInfoElement.innerHTML = planetInfoHtml;
+  document.getElementById('planetModal').style.display = 'block';
+}
+
+function setupModalCloseEvents() {
+  const modal = document.getElementById('planetModal');
+  document.querySelector('.close').addEventListener('click', () => {
+    modal.style.display = "none";
+  });
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
+
